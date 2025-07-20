dc2db778c36a00e45f4d809e69cd4d608ac831c3
Zexin Yuan <git@yzx9.xyz>
2025-07-20 13:24:15 +0800

    🔨 Build packages in nix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
   outputs =
-    { nixpkgs, ... }:
+    { self, nixpkgs, ... }:
 
     let
       inherit (nixpkgs) lib;
@@ -24,12 +24,68 @@
         "aarch64-darwin"
       ];
       forEachSupportedSystem = f: transposeAttrs (lib.genAttrs supportedSystems f);
+
+      aim-package =
+        {
+          lib,
+          stdenv,
+          rustPlatform,
+          installShellFiles,
+          testers,
+        }:
+
+        rustPlatform.buildRustPackage (finalAttrs: {
+          pname = "aim";
+          version = self.shortRev or self.dirtyShortRev or "unknown";
+
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./cli
+              ./core
+              ./Cargo.toml
+              ./Cargo.lock
+            ];
+          };
+
+          cargoLock = {
+            # NOTE: This is only used for Git dependencies
+            allowBuiltinFetchGit = true;
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = [ installShellFiles ];
+
+          postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+            installShellCompletion --cmd aim \
+              --bash <($out/bin/aim generate-completion bash) \
+              --fish <($out/bin/aim generate-completion fish) \
+              --zsh <($out/bin/aim generate-completion zsh)
+          '';
+
+          passthru.tests = {
+            version = testers.version {
+              package = finalAttrs.finalPackage;
+              version = finalAttrs.version;
+            };
+          };
+
+          meta = {
+            description = " Analyze. Interact. Manage Your Time";
+            homepage = "https://github.com/yzx9/aim";
+            license = lib.licenses.asl20;
+            platforms = lib.platforms.all;
+            maintainers = with lib.maintainers; [ yzx9 ];
+            mainProgram = "aim";
+          };
+        });
     in
     forEachSupportedSystem (
       system:
 
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        aim = pkgs.callPackage aim-package { };
       in
       {
         devShells.default = pkgs.mkShell {
@@ -45,6 +101,16 @@
             just
           ];
         };
+
+        packages = {
+          default = aim;
+          inherit aim;
+        };
       }
-    );
+    )
+    // {
+      overlays.default = final: _: {
+        aim = final.callPackage aim-package { };
+      };
+    };
 }
